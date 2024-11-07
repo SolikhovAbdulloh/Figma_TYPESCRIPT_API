aff75f8144c221241e11fc9b2c1339a3caa97916
SolihovAbdulloh <abdullasolikh@mail.com>
2024-11-07 10:44:52 +0500

    'finish

--- main.js
+++ main.js
@@ -30,14 +30,31 @@ window.addEventListener("click", (modal) => {
     if (modal.target === Modal)
         Modal.style.display = "none";
 });
-// const products: any[] = [];
-// async function Nation(url: string): Promise<any> {
-//   const respon = await fetch(url);
-//   const promise = await respon.json();
-//   //   console.log(promise);
-//   products.push(...promise);
-//   return promise;
-// }
-// Nation("http://localhost:3000/posts");
-// let card = JSON.stringify(localStorage.getItem("card")) || [];
+let api = "https://api.currencyapi.com/v3/latest?apikey=cur_live_5k3XE4VO37hh2y7knCAF7K3nzj5RTIAxVedvJyA4";
+async function Dollar(api) {
+    const respon = await fetch(api);
+    const promise = await respon.json();
+    return promise;
+}
+console.log(Dollar(api));
+const data = Dollar(api);
+const Usd = document.getElementById("USD");
+const Uzs = document.getElementById("UZS");
+async function displayCurrencyData() {
+    try {
+        const data = await Dollar(api);
+        const currencyOutput = document.getElementById("currency-output");
+        const usdRate = data.data.USD.value;
+        const UZSRate = data.data.UZS.value;
+        const UZB = UZSRate.toFixed(2);
+        if (Usd) {
+            Usd.innerHTML = `USD: ${usdRate} $ dollar <img src="../img/Flag_of_the_United_States.svg" alt=""> `;
+            Uzs.innerHTML = `UZS: ${UZB} So'm <img class="w-4 h-7" src="../img/flag-for-flag-uzbekistan-svgrepo-com.svg" alt="">`;
+        }
+    }
+    catch (error) {
+        console.error("Xatolik yuz berdi:", error);
+    }
+}
+displayCurrencyData();
 //# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -8,23 +8,21 @@ const Plus = document.querySelectorAll<HTMLButtonElement>(".pluss");
 //     let panel1 = panel.nextElementSibling as HTMLDivElement | null;
 //     if (panel1){
 //       panel1.style.display = panel1.style.display === "none" ? "block" : "none";
-      
+
 //     }
 //   });
 // });
 
-Plus.forEach((btn)=>{
-    btn.addEventListener("click",()=>{
-      let panel = btn.nextElementSibling as HTMLElement
-console.log(panel);
-      
-      
-      if(panel){
-        panel.style.display = panel.style.display === "none" ? "block" : "none";
-      }
+Plus.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    let panel = btn.nextElementSibling as HTMLElement;
+    console.log(panel);
 
-    })
-    })
+    if (panel) {
+      panel.style.display = panel.style.display === "none" ? "block" : "none";
+    }
+  });
+});
 
 Login_btn.addEventListener("click", () => {
   Modal.style.display = "block";
@@ -38,18 +36,38 @@ window.addEventListener("click", (modal) => {
   if (modal.target === Modal) Modal.style.display = "none";
 });
 
-// const products: any[] = [];
+let api =
+  "https://api.currencyapi.com/v3/latest?apikey=cur_live_5k3XE4VO37hh2y7knCAF7K3nzj5RTIAxVedvJyA4";
+async function Dollar(api: string): Promise<any> {
+  const respon = await fetch(api);
+  const promise = await respon.json();
 
-// async function Nation(url: string): Promise<any> {
-//   const respon = await fetch(url);
-//   const promise = await respon.json();
-//   //   console.log(promise);
+  return promise;
+}
 
-//   products.push(...promise);
+console.log(Dollar(api));
 
-//   return promise;
-// }
+const data: object = Dollar(api);
+const Usd = document.getElementById("USD") as HTMLElement;
+const Uzs = document.getElementById("UZS") as HTMLElement;
 
-// Nation("http://localhost:3000/posts");
+async function displayCurrencyData() {
+  try {
+    const data = await Dollar(api);
+    const currencyOutput = document.getElementById("currency-output");
 
-// let card = JSON.stringify(localStorage.getItem("card")) || [];
+    const usdRate = data.data.USD.value;
+    const UZSRate = data.data.UZS.value as Number;
+    const UZB = UZSRate.toFixed(2)
+    if (Usd) {
+      Usd.innerHTML = `USD: ${usdRate} $ dollar <img src="../img/Flag_of_the_United_States.svg" alt=""> `; 
+      Uzs.innerHTML = `UZS: ${UZB} So'm <img class="w-4 h-7" src="../img/flag-for-flag-uzbekistan-svgrepo-com.svg" alt="">`;
+    }
+    
+  } catch (error) {
+    console.error("Xatolik yuz berdi:", error);
+  }
+ 
+}
+
+displayCurrencyData()
